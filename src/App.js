fc6289b73669333a1d7cadbcdc9f3c1c91436ed1
rickfurt <ricardofurtado2609@gmail.com>
2019-03-11 13:33:46 +1100

    handle click function created

--- src/App.js
+++ src/App.js
@@ -2,27 +2,42 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state ={
+      key:''
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e){
+    this.setState({
+      key:e.target.id
+    })
+    console.log(this.state.key)
+  }
+  
   render() {
     return (
       <div className="container-fluid">
         <div id="drum-machine">
           <div className="row">
             <div className="col-12">
-              <button type="button" id="q" className="drum-pad btn btn-danger">Q
-                {/* <audio className='clip' id="q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio> */}
+              <button type="button" id="q" className="drum-pad btn btn-danger" onClick={this.handleClick}>
+              Q
               </button>
-              <button type="button" id="w" className="drum-pad btn btn-danger">W</button>
-              <button type="button" id="e" className="drum-pad btn btn-danger">E</button>
+              <button type="button" id="w" className="drum-pad btn btn-danger" onClick={this.handleClick}>W</button>
+              <button type="button" id="e" className="drum-pad btn btn-danger" onClick={this.handleClick}>E</button>
             </div>
             <div className="col-12">
-              <button type="button" id="a" className="drum-pad btn btn-danger">A</button>
-              <button type="button" id="s" className="drum-pad btn btn-danger">S</button>
-              <button type="button" id="d"className="drum-pad btn btn-danger">D</button>
+              <button type="button" id="a" className="drum-pad btn btn-danger" onClick={this.handleClick}>A</button>
+              <button type="button" id="s" className="drum-pad btn btn-danger" onClick={this.handleClick}>S</button>
+              <button type="button" id="d"className="drum-pad btn btn-danger" onClick={this.handleClick}>D</button>
             </div>
             <div className="col-12">
-              <button type="button" id="z" className="drum-pad btn btn-danger">Z</button>
-              <button type="button" id="x" className="drum-pad btn btn-danger">X</button>
-              <button type="button" id="c" className="drum-pad btn btn-danger">C</button>
+              <button type="button" id="z" className="drum-pad btn btn-danger" onClick={this.handleClick}>Z</button>
+              <button type="button" id="x" className="drum-pad btn btn-danger"onClick={this.handleClick}>X</button>
+              <button type="button" id="c" className="drum-pad btn btn-danger"onClick={this.handleClick}>C</button>
             </div>
           </div>        
       </div>
